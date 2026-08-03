fa7176953b2bca9e35ce52b526c611317ae45812
arthurLmre <45999149+arthurLmre@users.noreply.github.com>
2026-08-04 00:09:44 +0200

    chore: Add Router

---
+++ lib/features/city_search.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CitySearchPage extends StatelessWidget {
+  const CitySearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Recherche de ville')));
+  }
+}
