0eddd73cbbd84dbedb83d9c95cf6832a76776168
Arthur <arthurlamure01@gmail.com>
2026-08-04 00:08:51 +0200

    chore: Add Router

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,15 @@
+import 'package:go_router/go_router.dart';
+import 'package:weather_app/core/router/app_routes.dart';
+import 'package:weather_app/features/city_search.dart';
+
+abstract final class AppRouter {
+  static final router = GoRouter(
+    initialLocation: AppRoutes.citySearch,
+    routes: [
+      GoRoute(
+        path: AppRoutes.citySearch,
+        builder: (context, state) => const CitySearchPage(),
+      ),
+    ],
+  );
+}
