5518e9ded937a0f61327e3e94bdc644248bcf02e
Arthur <arthurlamure01@gmail.com>
2026-08-07 10:35:21 +0200

    ci: fix check.sh

--- tools/check.sh
+++ tools/check.sh
@@ -2,14 +2,22 @@
 
 set -e
 
+if command -v fvm >/dev/null 2>&1; then
+  DART_CMD="fvm dart"
+  FLUTTER_CMD="fvm flutter"
+else
+  DART_CMD="dart"
+  FLUTTER_CMD="flutter"
+fi
+
 echo "🔍 Vérification du formatage..."
-dart format \
+$DART_CMD format \
   --output=none \
   --set-exit-if-changed \
   lib test
 
 echo "🔎 Analyse du projet..."
-flutter analyze
+$FLUTTER_CMD analyze
 
 echo "🧪 Exécution des tests..."
-flutter test
+$FLUTTER_CMD test
