e9ae7e4e76997281518f40d6360fef44cc053904
Arthur <arthurlamure01@gmail.com>
2026-08-07 10:28:04 +0200

    ci: fix details

--- tools/check.sh
+++ tools/check.sh
@@ -3,13 +3,13 @@
 set -e
 
 echo "🔍 Vérification du formatage..."
-fvm dart format \
+dart format \
   --output=none \
   --set-exit-if-changed \
   lib test
 
 echo "🔎 Analyse du projet..."
-fvm flutter analyze
+flutter analyze
 
 echo "🧪 Exécution des tests..."
-fvm flutter test
+flutter test
